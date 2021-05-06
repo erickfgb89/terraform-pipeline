fe3a0775231b721c2e55580a454e791cd9a52b61
Erick Aldaz <erick.aldaz@coxautoinc.com>
2021-05-05 20:21:00 -0400

    ealdaz - reordering super calls

--- src/TerraformPluginVersion14.groovy
+++ src/TerraformPluginVersion14.groovy
@@ -1,25 +1,25 @@
 class TerraformPluginVersion14 extends TerraformPluginVersion12 {
     @Override
     public void apply(TerraformInitCommand command) {
-        super.apply(command)
         command.withGlobalDirectoryOption()
+        super.apply(command)
     }
 
     @Override
     public void apply(TerraformValidateCommand command) {
-        super.apply(command)
         command.withGlobalDirectoryOption()
+        super.apply(command)
     }
 
     @Override
     public void apply(TerraformPlanCommand command) {
-        super.apply(command)
         command.withGlobalDirectoryOption()
+        super.apply(command)
     }
 
     @Override
     public void apply(TerraformApplyCommand command) {
-        super.apply(command)
         command.withGlobalDirectoryOption()
+        super.apply(command)
     }
 }
