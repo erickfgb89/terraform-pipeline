87895051a06a78eb1bab984d31ab152ab74d7f69
Erick Aldaz <erick.aldaz@coxautoinc.com>
2021-05-05 20:16:07 -0400

    ealdaz - extending v12 plugin

--- src/TerraformPluginVersion14.groovy
+++ src/TerraformPluginVersion14.groovy
@@ -1,21 +1,25 @@
-class TerraformPluginVersion14 extends TerraformPluginVersion {
+class TerraformPluginVersion14 extends TerraformPluginVersion12 {
     @Override
     public void apply(TerraformInitCommand command) {
+        super.apply(command)
         command.withGlobalDirectoryOption()
     }
 
     @Override
     public void apply(TerraformValidateCommand command) {
+        super.apply(command)
         command.withGlobalDirectoryOption()
     }
 
     @Override
     public void apply(TerraformPlanCommand command) {
+        super.apply(command)
         command.withGlobalDirectoryOption()
     }
 
     @Override
     public void apply(TerraformApplyCommand command) {
+        super.apply(command)
         command.withGlobalDirectoryOption()
     }
 }
